d77ad1e602416eaf0dba6f8bef133276676a95dd
Binner <lucian.binner@hotmail.com>
2021-01-03 11:38:13 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '678c618683ba207c596fd349d6097d3d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3670fdb9-fdad-463e-bad4-31e5407d5ad2'
+};
