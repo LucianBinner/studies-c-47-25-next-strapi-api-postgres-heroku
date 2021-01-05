d4fce86643395bd364fd78c8783550f739c26aec
Binner <lucian.binner@hotmail.com>
2021-01-05 17:19:46 -0300

    add lifecycle-hooks

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -8,10 +8,12 @@ const axios = require('axios');
  */
 
 module.exports = {
-  async afterCreate(result, data) {
-    axios.post('https://api.netlify.com/build_hooks/5ff47573b22751013c824921');
-  },
-  async afterUpdate(result, params, data) {
-    axios.post('https://api.netlify.com/build_hooks/5ff47573b22751013c824921');
-  },
+  lifecycles: {
+    async afterCreate(result, data) {
+      axios.post('https://api.netlify.com/build_hooks/5ff47573b22751013c824921');
+    },
+    async afterUpdate(result, params, data) {
+      axios.post('https://api.netlify.com/build_hooks/5ff47573b22751013c824921');
+    },
+  }
 };
